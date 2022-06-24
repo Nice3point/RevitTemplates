c85bd7ef1c39f4c47033bf967d172e86f9999915
Nice3point <nice3point@gmail.com>
2022-06-24 11:56:34 +0300

    Add Microsoft.Xaml.Behaviors

--- Templates/Nice3point.Revit.AddIn/Application.cs
+++ Templates/Nice3point.Revit.AddIn/Application.cs
@@ -8,17 +8,33 @@ public class Application : IExternalApplication
 {
     public Result OnStartup(UIControlledApplication application)
     {
-        var panel = application.CreatePanel("Panel name", "Nice3point.Revit.AddIn");
-
-        var showButton = panel.AddPushButton<Command>("Button text");
-        showButton.SetImage("/Nice3point.Revit.AddIn;component/Resources/Icons/RibbonIcon16.png");
-        showButton.SetLargeImage("/Nice3point.Revit.AddIn;component/Resources/Icons/RibbonIcon32.png");
-
+        ForceLoadLibraries();
+        CreateRibbon(application);
         return Result.Succeeded;
     }
 
     public Result OnShutdown(UIControlledApplication application)
     {
         return Result.Succeeded;
+    }
+
+    private static void CreateRibbon(UIControlledApplication application)
+    {
+        var panel = application.CreatePanel("Panel name", "Nice3point.Revit.AddIn");
+
+        var showButton = panel.AddPushButton<Command>("Button text");
+        showButton.SetImage("/Nice3point.Revit.AddIn;component/Resources/Icons/RibbonIcon16.png");
+        showButton.SetLargeImage("/Nice3point.Revit.AddIn;component/Resources/Icons/RibbonIcon32.png");
+    }
+
+    private static void ForceLoadLibraries()
+    {
+        var assemblies = new List<string>
+        {
+            "Microsoft.Xaml.Behaviors",
+            "System.ComponentModel.Annotations"
+        };
+
+        foreach (var assembly in assemblies) AppDomain.CurrentDomain.Load(assembly);
     }
 }
