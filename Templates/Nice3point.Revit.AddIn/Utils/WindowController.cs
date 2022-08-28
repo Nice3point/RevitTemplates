384c0a2aa84feb0dcd1f26212acf99f0d70b5c34
Nice3point <nice3point@gmail.com>
2022-08-28 13:17:18 +0300

    Hide, Close, Show for WindowController

--- Templates/Nice3point.Revit.AddIn/Utils/WindowController.cs
+++ Templates/Nice3point.Revit.AddIn/Utils/WindowController.cs
@@ -6,25 +6,6 @@ public static class WindowController
 {
     private static readonly List<Window> Windows = new();
 
-    /// <summary>
-    ///     Attempts to set focus to this element
-    /// </summary>
-    /// <typeparam name="T">Type of window</typeparam>
-    /// <returns>True if the window instance has already been created</returns>
-    public static bool Focus<T>() where T : Window
-    {
-        var type = typeof(T);
-        foreach (var window in Windows)
-            if (window.GetType() == type)
-            {
-                if (window.WindowState == WindowState.Minimized) window.WindowState = WindowState.Normal;
-                window.Focus();
-                return true;
-            }
-
-        return false;
-    }
-
     /// <summary>Opens a window and returns without waiting for the newly opened window to close</summary>
     public static void Show(Window window)
     {
@@ -39,6 +20,64 @@ public static class WindowController
     {
         Attach(window);
         window.Show(handle);
+    }
+
+    /// <summary>
+    ///     Makes a window visible
+    /// </summary>
+    /// <typeparam name="T">Type of <see cref="T:System.Windows.Window" /></typeparam>
+    public static void Show<T>() where T : Window
+    {
+        var type = typeof(T);
+        foreach (var window in Windows)
+            if (window.GetType() == type)
+                window.Show();
+    }
+
+    /// <summary>
+    ///     Makes a window invisible
+    /// </summary>
+    /// <typeparam name="T">Type of <see cref="T:System.Windows.Window" /></typeparam>
+    public static void Hide<T>() where T : Window
+    {
+        var type = typeof(T);
+        foreach (var window in Windows)
+            if (window.GetType() == type)
+                window.Hide();
+    }
+
+    /// <summary>
+    ///     Manually closes a <see cref="T:System.Windows.Window" />
+    /// </summary>
+    /// <typeparam name="T">Type of window</typeparam>
+    public static void Close<T>() where T : Window
+    {
+        var type = typeof(T);
+        for (var i = Windows.Count - 1; i >= 0; i--)
+        {
+            var window = Windows[i];
+            if (window.GetType() == type)
+                window.Close();
+        }
+    }
+
+    /// <summary>
+    ///     Attempts to set focus to this element
+    /// </summary>
+    /// <typeparam name="T">Type of <see cref="T:System.Windows.Window" /></typeparam>
+    /// <returns>True if the window instance has already been created</returns>
+    public static bool Focus<T>() where T : Window
+    {
+        var type = typeof(T);
+        foreach (var window in Windows)
+            if (window.GetType() == type)
+            {
+                if (window.WindowState == WindowState.Minimized) window.WindowState = WindowState.Normal;
+                window.Focus();
+                return true;
+            }
+
+        return false;
     }
 
     private static void Attach(Window window)
