6ab76b0f9e67493c6f9a055e8b1d54091514cbde
Nice3point <nice3point@gmail.com>
2023-05-21 18:05:04 +0300

    Fix usings

--- Templates/Nice3point.Revit.Solution/Build/Build.Installer.cs
+++ Templates/Nice3point.Revit.Solution/Build/Build.Installer.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 using Nuke.Common.Git;
+using Nuke.Common.Utilities;
 using Serilog.Events;
 
 sealed partial class Build
