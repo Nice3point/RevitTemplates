7dbae1203b129012298b55235f9aea434b84eec1
Nice3point <nice3point@gmail.com>
2024-01-11 13:51:33 +0300

    Bump version

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,7 @@
+# 3.2.1
+
+- Fix Github release version validation
+
 # 3.2.0
 
 - .NET 8 support.
--- build/Build.Configuration.cs
+++ build/Build.Configuration.cs
@@ -1,6 +1,6 @@
 ﻿sealed partial class Build
 {
-    const string Version = "3.2.0";
+    const string Version = "3.2.1";
     readonly AbsolutePath ArtifactsDirectory = RootDirectory / "output";
     readonly AbsolutePath ChangeLogPath = RootDirectory / "Changelog.md";
 
