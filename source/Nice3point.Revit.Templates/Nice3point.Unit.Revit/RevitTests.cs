2201b288289e5c44a715185195bbad75a22de876
Nice3point <nice3point@gmail.com>
2025-12-10 21:55:11 +0300

    [Templates] Add Revit unit test template with TUnit

---
+++ source/Nice3point.Revit.Templates/Nice3point.Unit.Revit/RevitTests.cs
@@ -0,0 +1,30 @@
+﻿using Nice3point.TUnit.Revit;
+using Nice3point.TUnit.Revit.Executors;
+using TUnit.Core.Executors;
+
+namespace Nice3point.Unit.Revit;
+
+public sealed class RevitTests : RevitApiTest
+{
+    private static Document _documentFile = null!;
+
+    [Before(Class)]
+    [HookExecutor<RevitThreadExecutor>]
+    public static void Setup()
+    {
+        _documentFile = Application.OpenDocumentFile("");
+    }
+
+    [After(Class)]
+    [HookExecutor<RevitThreadExecutor>]
+    public static void Cleanup()
+    {
+        _documentFile.Close(false);
+    }
+
+    [Test]
+    [TestExecutor<RevitThreadExecutor>]
+    public async Task RevitTest()
+    {
+    }
+}
