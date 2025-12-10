3eacdda750d311404d65823773292109e6c7eba0
Nice3point <nice3point@gmail.com>
2025-12-10 21:55:19 +0300

    [Templates] Add Revit benchmark template with BenchmarkDotNet

---
+++ source/Nice3point.Revit.Templates/Nice3point.Benchmark.Revit/Benchmarks/RevitBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Nice3point.Benchmark.Revit.Benchmarks;
+
+[MemoryDiagnoser]
+public class RevitBenchmarks : RevitApiBenchmark
+{
+    private Document _documentFile = null!;
+
+    protected sealed override void OnSetup()
+    {
+        _documentFile = Application.OpenDocumentFile("");
+    }
+    
+    protected sealed override void OnCleanup()
+    {
+        _documentFile.Close(false);
+    }
+
+    [Benchmark]
+    public void Benchmark()
+    {
+    }
+}
---
+++ source/Nice3point.Revit.Templates/Nice3point.Benchmark.Revit/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Nice3point.BenchmarkDotNet.Revit;
+using Nice3point.Benchmark.Revit.Benchmarks;
+
+var configuration = ManualConfig.Create(DefaultConfig.Instance)
+    .AddJob(Job.Default.WithCurrentConfiguration());
+
+BenchmarkRunner.Run<RevitBenchmarks>(configuration);
