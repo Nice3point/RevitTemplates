b54ede22dbb8d0ef47daf44a0e42d0bc98d4e751
Nice3point <nice3point@gmail.com>
2025-12-28 18:42:03 +0300

    [Build] Remove required attribute from `Version` property in build configuration options.

--- build/Options/BuildOptions.cs
+++ build/Options/BuildOptions.cs
@@ -5,6 +5,6 @@ namespace Build.Options;
 [Serializable]
 public sealed record BuildOptions
 {
-    [Required] public string Version { get; init; } = "1.0.0";
+    public string Version { get; init; } = "1.0.0";
     [Required] public string OutputDirectory { get; init; } = null!;
 }
